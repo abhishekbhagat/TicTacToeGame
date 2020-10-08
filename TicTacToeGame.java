bc5b6956fd0ec170f8e928f74a99ed785a611e37
Abhishek bhagat <abhishekbhagatraw@gmail.com>
2020-10-08 11:03:50 +0530

    [abhishek] add: created a showBoard method to show board

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class TicTacToeGame {
 
-	public static void createBoard() {
+	public static char[] createBoard() {
 		char[] board = new char[10];
 		for (int i = 0; i < 10; i++) {
 			board[i] = ' ';
 		}
-
+		return board;
 	}
 
 	public static char chooseLetter(char playerLetter) {
@@ -21,12 +21,24 @@ public class TicTacToeGame {
 		return computerLetter;
 	}
 
+	public static void showBoard(char board[]) {
+		for (int cell = 0; cell < 9; cell++) {
+			if (cell == 3 || cell == 6)
+				System.out.println("\n");
+			if (cell == 2 || cell == 5 || cell == 8)
+				System.out.println(board[cell]);
+			else
+				System.out.print(board[cell] + " |");
+		}
+	}
+
 	public static void main(String[] args) {
-		createBoard();
+		char board[] = createBoard();
 		Scanner letter = new Scanner(System.in);
 		System.out.println("Player please choose a letter X OR O ");
 		char playerLetter = letter.next().charAt(0);
-		char computerLetter=chooseLetter(playerLetter);
+		char computerLetter = chooseLetter(playerLetter);
+		showBoard(board);
 	}
 
 }

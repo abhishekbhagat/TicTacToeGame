4ea28988dcae47ca0d4c5b6baf3c3be3f2b5d0b1
Abhishek bhagat <abhishekbhagatraw@gmail.com>
2020-10-08 10:33:21 +0530

    [abhishek] add : create a method to choose letter for player an computer

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -3,7 +3,7 @@ package com.bridgelabz.workshop;
 import java.util.Scanner;
 
 public class TicTacToeGame {
-	// uc1 method
+
 	public static void createBoard() {
 		char[] board = new char[10];
 		for (int i = 0; i < 10; i++) {
@@ -12,8 +12,21 @@ public class TicTacToeGame {
 
 	}
 
+	public static char chooseLetter(char playerLetter) {
+		char computerLetter;
+		if (playerLetter == 'X')
+			computerLetter = 'O';
+		else
+			computerLetter = 'X';
+		return computerLetter;
+	}
+
 	public static void main(String[] args) {
 		createBoard();
+		Scanner letter = new Scanner(System.in);
+		System.out.println("Player please choose a letter X OR O ");
+		char playerLetter = letter.next().charAt(0);
+		char computerLetter=chooseLetter(playerLetter);
 	}
 
 }
